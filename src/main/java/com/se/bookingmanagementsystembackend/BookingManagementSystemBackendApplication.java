8d46681cbcbac4c3946336dedae58815865dc46c
sohank07 <kakatkarsohan7@gmail.com>
2023-02-18 19:43:32 -0500

    initial commit

---
+++ src/main/java/com/se/bookingmanagementsystembackend/BookingManagementSystemBackendApplication.java
@@ -0,0 +1,14 @@
+package com.se.bookingmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!!");
+		SpringApplication.run(BookingManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/bookingmanagementsystembackend/BookingManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.bookingmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
